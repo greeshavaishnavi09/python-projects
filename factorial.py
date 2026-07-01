7d26c49fc2d8e7d180297c8bc851b3aeca1ab6f7
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-01 10:33:38 +0530

    Initial commit

---
+++ factorial.py
@@ -0,0 +1,2 @@
+num = 6
+print("number is factorial" if n<0: result is false result =1 for i in range(1,n+1): if n*i>0: result true(num) )
---
+++ reverse.py
@@ -0,0 +1,3 @@
+message = "Python is awesome"
+reversed_sentence =" " .join(message.split()[::-1])
+print(reversed_sentence)
