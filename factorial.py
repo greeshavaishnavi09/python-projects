e06ffcc819132a83761fd7ffcfb378496001b247
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-01 14:19:41 +0530

    Initial commit

--- factorial.py
+++ factorial.py
@@ -1,2 +1,5 @@
-num = 6
-print("number is factorial" if n<0: result is false result =1 for i in range(1,n+1): if n*i>0: result true(num) )
+num = 5
+fact = 1
+for n in range(1,num+1):
+    fact = fact*n
+print(fact)    
