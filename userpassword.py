9b3e5c34fea9e1a28a985368f24b62e8579f68b4
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-13 12:59:27 +0530

    Initial commit

---
+++ userpassword.py
@@ -0,0 +1,13 @@
+user_name = input("Enter the user name:")
+password = int(input("Enter the password:"))
+
+user_name1 = "Vaishnavi"
+password1 = 2567
+
+if user_name ==user_name1 :
+    if password == password1:
+        print("Login successfull")
+    else:
+        print("Invalid password") 
+else:
+    print("Invalid username")           
---
+++ validpin.py
@@ -0,0 +1,9 @@
+pin = 3426
+balance = 3000
+if pin == pin:
+    if balance >= 3000:
+        print("transaction succussfull")
+    else:
+        print("transaction not successfull")  
+else:
+    print("Invalid pin")          
